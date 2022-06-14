b9d55e421ae483117ceba6bc85b782c4b70a6ce8
Kwanghoo Lee <gggo1313@gmail.com>
2022-06-14 15:23:16 +0900

    정규식 부분 풀이 추가"

--- countAttraction.js
+++ countAttraction.js
@@ -10,3 +10,11 @@ function solution (price, money, count) {
 };
 
 console.log(solution(3, 20, 4));
+
+// 다른 사람 풀이
+// 정규식
+const solution = (..._) => Math.max(_[0] * _[2] * ++_[2] / 2 - _[1], 0);
+// (..._) : solution 함수에 들어오는 매개변수의 목록
+// _[0] 매개변수 목록의 배열에서 index 0번째
+// ++_[2] 는 _[2]에 대해 먼저 +1 하고 계산
+// Math.max(total_price - money, 0)으로 삼항 연산을 최대값 고르는 방법으로 대체
