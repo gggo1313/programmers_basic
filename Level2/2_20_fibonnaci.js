f9df93427aaf06b2720faa1dfc1e826990fd4c5a
Kwanghoo Lee <gggo1313@gmail.com>
2022-07-21 00:20:50 +0900

    n번째 피보나치수 구하기 // 바이트 수에 따른 오버플로우 고려하기

---
+++ Level2/2_20_fibonnaci.js
@@ -0,0 +1,19 @@
+// 2 이상의 n을 입력했을 때 n번째 피보나치수를 1234567로 나눈 나머지를 return
+
+
+
+function solution (n) {
+    const fibo = [0, 1];
+
+    for (let i = 1; i < n; i++) {
+        let leng = fibo.length;
+        let next = fibo[leng - 2] + fibo[leng - 1];
+        fibo.push(next % 1234567);
+    };
+
+    const answer = fibo[n];
+
+    return answer;
+};
+
+console.log(solution(5))
