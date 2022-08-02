de013d607791c9bb119ad959b5a974d8a19fc370
Kwanghoo Lee <gggo1313@gmail.com>
2022-08-02 21:11:16 +0900

    던전 피로도를 이용하여 최대 탐험 가능 던전 수 찾기 // dfs를 이용한 모든 조합 탐색 // 완전탐색

---
+++ Level2/2_33_dungeonFatigue.js
@@ -0,0 +1,26 @@
+// 던전마다 [입장 최소 필요 피로도, 소모 피로도]가 담긴 배열 dungeons
+// 현재 피로도 k가 주어질 때, 최대로 탐험할 수 있는 던전 수를 return
+function solution (k, dungeons) {
+    const answer = [];
+    const visited = new Array(dungeons.length).fill(false);
+
+    function dfs(count, k) {
+        answer.push(count);
+
+        for (let i = 0; i < dungeons.length; i++) {
+            let current = dungeons[i];
+            if (k >= current[0] && !visited[i]) {
+                visited[i] = true;
+                dfs(count + 1, k - current[1]);
+                visited[i] = 0;
+            };
+        };
+    };
+
+    dfs(0, k);
+
+    return Math.max(...answer);
+};
+
+console.log(solution(80, [[80, 20], [50, 40], [30, 10]]));
+console.log(solution(40, [[40, 20], [10, 10], [10, 10], [10, 10], [10, 10]]));
